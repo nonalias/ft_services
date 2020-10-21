d00a989c748544e03e540059030fe7097617240b
Taehoon Kim <taehkim@c5r1s3.42seoul.kr>
2020-10-21 09:12:46 +0900

    setup.sh: port 433 added

--- setup.sh
+++ setup.sh
@@ -4,10 +4,14 @@ docker rmi -f $(docker images -q)
 # remote all containers
 docker rm -f $(docker ps -alq)
 
+# starting minikube
 minikube start --driver=virtualbox
 
+# Nginx operation
 cd ./srcs/nginx/
 docker build -t nginx-image .
-# docker run -d -p 80:80 nginx-image nginx
+# docker run -d -p 80:80 -p 443:443 nginx-image nginx
 # kubectl create deployment --image=nginx-image
 cd ../..
+
+
