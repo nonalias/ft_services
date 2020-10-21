7a754e4c7ba4fcae6c9ba2c35f004b50542f6ad4
Taehoon Kim <taehkim@c5r1s3.42seoul.kr>
2020-10-21 18:29:16 +0900

    metallb added

--- clean.sh
+++ clean.sh
@@ -1,3 +1,7 @@
 cd srcs/nginx/
 kubectl delete -f *.yaml
 cd ../../
+
+cd srcs/metallb/
+kubectl delete -f *.yaml
+cd ../..
--- setup.sh
+++ setup.sh
@@ -4,6 +4,11 @@ minikube start --driver=virtualbox
 # changing docker image location
 eval $(minikube docker-env)
 
+# metallb operation
+cd ./srcs/metallb/
+kubectl apply -f metallb.yaml
+cd ../..
+
 # Nginx operation
 cd ./srcs/nginx/
 docker build -t nginx-image .
