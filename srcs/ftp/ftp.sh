5552298ab0fcb4af42e1f1de1b0b89079f42c0cd
Taehoon Kim <taehkim@c5r1s3.42seoul.kr>
2020-11-04 22:04:17 +0900

    ftp: 경로변경

--- srcs/ftp/ftp.sh
+++ srcs/ftp/ftp.sh
@@ -1,7 +1,7 @@
-mkdir -p /home/vsftpd/$USER
+mkdir -p /ftps/$USER
 addgroup -g 433 -S $USER
-adduser -u 431 -D -G $USER -h /home/vsftpd/$USER -s /bin/false $USER
+adduser -u 431 -D -G $USER -h /ftps/$USER -s /bin/false $USER
 echo "$USER:$PASSWORD" | chpasswd
-chown -R $USER:$USER /home/vsftpd/$USER
+chown -R $USER:$USER /ftps/$USER
 
 /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
