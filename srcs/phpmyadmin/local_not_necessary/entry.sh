98acb6029864d0a8e668d9ffe8b1d989ea136bf3
Taehoon Kim <taehkim@c5r1s3.42seoul.kr>
2020-11-13 15:56:05 +0900

    일단 pvc는 생성, 하지만 nginx/phpmyadmin으로 로그인했을 때 경로를 못 찾음

--- srcs/phpmyadmin/local_not_necessary/entry.sh
+++ srcs/phpmyadmin/local_not_necessary/entry.sh
@@ -1,5 +1,12 @@
 #mkdir -p /var/www/html/phpmyadmin/tmp
 #adduser -D -g www www
 #chown -R www:www /var/www/html
+mkdir -p /var/www/html
+wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.tar.gz
+tar -xvf phpMyAdmin-5.0.2-all-languages.tar.gz
+rm -f phpMyAdmin-5.0.2-all-languages.tar.gz
+mv phpMyAdmin-5.0.2-all-languages phpmyadmin
+mv /phpmyadmin/* /var/www/html/phpmyadmin
+cp /tmp/config.inc.php /var/www/html/phpmyadmin
 /usr/sbin/php-fpm7
 nginx -g 'pid /tmp/nginx.pid; daemon off;'
