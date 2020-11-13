9876bc058a7fe62458e8778b0e1aecd1ecebc162
Taehoon Kim <taehkim@c5r1s3.42seoul.kr>
2020-11-13 17:43:56 +0900

    config.int 절대경로 추가

--- srcs/phpmyadmin/config.inc.php
+++ srcs/phpmyadmin/config.inc.php
@@ -8,6 +8,7 @@ $cfg['Servers'][$i]['auth_type'] = 'cookie';
 $cfg['Servers'][$i]['host'] = 'mysql-service';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
+$cfg['PmaAbsoluteUri'] = '/phpmyadmin/';
 
 $cfg['UploadDir'] = '';
 $cfg['SaveDir'] = '';
